62208e616c7ec8ea835ec507a323202355ce37b1
zjpeng <1528580043@qq.com>
2018-05-06 12:54:55 +0800

    完成除disconnect外的基本功能

--- lcx.py
+++ lcx.py
@@ -7,6 +7,18 @@ from enum import Enum
 com_type=Enum('com_type',('chap_salt','chap_hash','chap_result','bind_request','bind_response','connect_request',
 'connect_response','data','disconnect'))
 
+requestID_count=0
+connectID_count=0
+recv_buffs=dict()
+send_buffs=dict()
+id_to_port=[]
+port_to_id=dict()
+req_to_port=[]
+recv_locks=dict()
+send_locks=dict()
+connected=dict()
+requesting=dict()
+
 #协议编码格式
 fmt_chap_salt='HBBs'
 fmt_chap_hash='HBBsBs'
@@ -28,9 +40,9 @@ class slave_args:
     tunnel_add=''
     usr_name=''
     usr_pw=''
-    remote_port=0
-    local_port=0
-    local_add=''
+    remote_ports=[]
+    local_host_ports=[]
+    port_pair=dict()
 
 l_args=listen_args()
 s_args=slave_args()
@@ -120,7 +132,25 @@ async def handle_slave(reader,writer):
     while chap(reader,writer)==False:
         pass
 
+    requestID_count=0
     while True:
+        for port,v in connected:
+            if v==True:
+                await recv_locks[port]
+                try:
+                    if len(recv_buffs[port])!=0:
+                        msg=struct.pack(fmt_data,7+len(recv_buffs[port]),com_type.data,port_to_id[port],len(recv_buffs[port]),recv_buffs[port])
+                        writer.write(msg)
+                        writer.drain()
+                finally:
+                    recv_locks[port].release()
+        for port,v in requesting:            
+            if v==True:
+                msg=struct.pack(fmt_connect_request,7,com_type.connect_request,requestID_count,port)
+                requestID_count=requestID_count+1
+                writer.write(msg)
+                await writer.drain()
+
         length=struct.unpack('H',await reader.readexactly(2))
         command=struct.unpack('B',await reader.readexactly(1))
         if command==com_type.bind_request:
@@ -134,37 +164,54 @@ async def handle_slave(reader,writer):
                 except asyncio.TimeoutError:
                     msg=struct.pack(fmt_bind_respone,8,com_type.bind_response,requestId,0,bind_port)
                 writer.write(msg)
+                recv_locks[bind_port]=asyncio.locks()
+                send_locks[bind_port]=asyncio.locks()
         elif command==com_type.connect_response:
-            pass
+            requestID=struct.unpack('H',await reader.readexactly(2))
+            result=struct.unpack('B',await reader.readexactly(1))
+            if result==1:
+                connectID=struct.unpack('H',await reader.readexactly(2))
+                port=req_to_port[requestID]
+                connected[port]=True
+                port_to_id[port]=connectID
+            else:
+                await reader.readexactly(2)
         elif command==com_type.data:
-            pass
+            connectID=struct.unpack('H',await reader.readexactly(2))
+            data_len=struct.unpack('H',await reader.readexactly(2))
+            data=struct.unpack('H',await reader.readexactly(data_len))
+            port=id_to_port[connectID]
+            await send_locks[port]
+            try:
+                send_buffs[port].append(data)
+            finally:
+                send_locks[port].release()
         elif command==com_type.disconnect:
-            pass
-async def handle_client():
-    pass 
+            server.close()
+            loop.run_until_complete(server.wait_closed())
+            return
+
+async def handle_client(reader,writer):
+    peer_host, peer_port, = writer.get_extra_info('peername')
+    sock_host, sock_port, = writer.get_extra_info('sockname')
     
+    requesting[sock_port]=True
+    while(True):
+        await send_locks[sock_port]
+        try:
+            if len(send_buffs[sock_port])!=0:
+                writer.write(send_buffs[sock_port])
+                await writer.drain()
+                send_buffs[sock_port].clear()
+        finally:
+            send_locks[sock_port].release()
 
-
-
-#与remote listen建立隧道连接
-async def build_tunnel(port,user,remoteAdd,reader,writer,loop):
-    ip,remotePort=remoteAdd.split(':')
-    name,password=user.split(':')
-
-    reader, writer=await asyncio.open_connection(ip,int(remotePort),loop=loop)
-    salt=await reader.read(10)
-
-    hashResult = hash(password+salt.decode())
-    answer=name+'|'+hashResult
-    await writer.write(answer.encode())
-
-    succeed = await reader.read(10)
-    if succeed.decode()=='1':
-        await writer.write(port.encode())
-        return port
-    else:
-        print("用户名或密码错误")
-        exit(2)
+        await recv_locks[sock_port]
+        try:
+            recv_buffs[sock_port].append(await reader.read(100))
+        finally:
+            recv_locks[sock_port].release()
+    
 
 def slave(opts,args):
     #获取参数
@@ -174,10 +221,11 @@ def slave(opts,args):
         elif opt == '-u':
             s_args.usr_name,s_args.usr_pw=arg.split(':')
         elif opt == '-p':
-            s_args.remote_port=int(arg)
+            s_args.remote_port=[int(p) for p in arg.split(',')]
         elif opt == '-l':
-            s_args.local_add,s_args.local_port=arg.split(':')
-    s_args.local_port=int(s_args.local_port)
+            s_args.local_host_ports=[p.split(':') for p in arg.split(',')]
+    for i in range(len(s_args.remote_ports)):
+        s_args.port_pair[s_args.remote_ports[i]]=int(s_args.local_host_ports[i][1])
     s_args.tunnel_port=int(s_args.tunnel_port)
 
     loop=asyncio.get_event_loop()
@@ -185,6 +233,8 @@ def slave(opts,args):
     
 async def handle_listen():
     reader,writer=await asyncio.open_connection(s_args.tunnel_add,s_args.tunnel_port,loop=loop)
+    
+    connectID_count=0
 
     length=struct.unpack('H',await reader.readexactly(2))
     command=struct.unpack('B',await reader.readexactly(1))
@@ -205,23 +255,77 @@ async def handle_listen():
     if(result==0):
         sys.exit(2)
 
+    #msg=struct.pack(fmt_bind_request,7,com_type.bind_request,1,)
     while True:
-        length=struct.unpack('H',await reader.readexactly(2))
-        command=struct.unpack('B',await reader.readexactly(1)) 
-    
+        for port,v in connected:            
+            if v==True:
+                msg=struct.pack(fmt_connect_request,8,com_type.connect_response,connectID_count,port)
+                connectID_count=connectID_count+1
+                writer.write(msg)
+                await writer.drain()
 
-if __name__ == "__main__":
-    try:
-        opts,args=getopt.getopt(sys.argv[1:],"m:p:u:r:l:",[])
-    except getopt.GetoptError:
-        print("lcx.py -m <runtype>")
-        sys.exit(2)
-    for opt,arg in opts:
-        if opt == "-m":
-            if arg == "listen":
-                listen(opts,args)
-            if arg == "slave":
-                slave(opts,args)
-            else:
-                print("use \'-m <type>\' to specify the type to execute.")
-                exit(2)
+        length=struct.unpack('H',await reader.readexactly(2))
+        command=struct.unpack('B',await reader.readexactly(1))
+        if command==com_type.bind_response:
+            requestID=struct.unpack('H',await reader.readexactly(2))  
+            result==struct.unpack('B',await reader.readexactly(1))
+            
+            await reader.readexactly(3)
+        elif command==com_type.connect_request:
+            requestID=struct.unpack('H',await reader.readexactly(2))
+            listen_port=struct.unpack('H',await reader.readexactly(2))
+            
+            client=asyncio.wait_for(asyncio.ensure_future(handle_server(connectID_count,s_args.port_pair[listen_port])),None)
+        elif command==com_type.data:
+            connectID=struct.unpack('H',await reader.readexactly(2))
+            data_len=struct.unpack('H',await reader.readexactly(2))
+            data=struct.unpack('H',await reader.readexactly(data_len))
+            await send_locks[connectID]
+            try:
+                send_buffs[connectID].append(data)
+            finally:
+                send_locks[connectID].release()
+        elif command==com_type.disconnect:
+            return
+            
+                  
+    
+async def handle_server(id,request_port):
+    reader,writer=await asyncio.open_connection(s_args.local_host_ports[0][0],request_port,loop=loop)
+
+    connected[request_port]=True
+
+    while(True):
+        await send_locks[request_port]
+        try:
+            if len(send_buffs[request_port])!=0:
+                writer.write(send_buffs[request_port])
+                await writer.drain()
+                send_buffs[request_port].clear()
+        finally:
+            send_locks[request_port].release()
+
+        await recv_locks[request_port]
+        try:
+            recv_buffs[request_port].append(await reader.read(100))
+        finally:
+            recv_locks[request_port].release()
+        
+
+
+#if __name__ == "__main__":
+try:
+    opts,args=getopt.getopt(sys.argv[1:],"m:p:u:r:l:",[])
+except getopt.GetoptError:
+    print("lcx.py -m <runtype>")
+    sys.exit(2)
+
+for opt,arg in opts:
+    if opt == "-m":
+        if arg == "listen":
+            listen(opts,args)
+        if arg == "slave":
+            slave(opts,args)
+        else:
+            print("use \'-m <type>\' to specify the type to execute.")
+            exit(2)
